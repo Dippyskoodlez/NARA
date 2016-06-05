b28fcfb0b79a07a0be04cdacf046c3eb8b79cb9b
Dippyskoodlez <sabilleg@gmail.com>
2016-06-04 20:12:25 -0500

    Total code rework
    Total code reworked for Qik use instead of Adafruit motor driver.

--- NaraBot/NaraBot.ino
+++ NaraBot/NaraBot.ino
@@ -1,160 +1,62 @@
   /*
-   * Current code is legacy NARA for reference and porting purposes only.
-   * 
-   * 
+   * N.A.R.A.
+   * This file is the primary software for the Arduino controlling the robot. 
    */
-  
-  
-  //-------------------------------------------------------// 
-  #include <AFMotor.h> //Adafruit motor header library: http://www.adafruit.com/products/81
+/*-------------------PINOUTS-------------------------------*/
+/*
+Required connections between Arduino and qik 2s12v10:
 
-//Motor Layout (Wiring specific)
-  AF_DCMotor motorFL(4, MOTOR12_64KHZ);
-  AF_DCMotor motorBL(1, MOTOR12_64KHZ); 
-  AF_DCMotor motorFR(2, MOTOR12_64KHZ);  
-  AF_DCMotor motorBR(3, MOTOR12_64KHZ);
+      Arduino   qik 2s12v10
+---------------------------
+          GND - GND
+Digital Pin 2 - TX
+Digital Pin 3 - RX
+Digital Pin 4 - RESET
 
-  char rcvDirection = 0; // Forward (F), Backward(B), Neutral steer(N)
-  int rcvSpeedL = 0; //0-255
+DO NOT connect the 5V output on the Arduino to the 5V output on the qik 2s12v10!
+*/
+/*-------------------END PINOUTS-------------------------------*/
+  
+#include <SoftwareSerial.h>
+#include <PololuQik.h>
+
+//Serial connections on pins 2,3,4
+PololuQik2s12v10 qik(2, 3, 4);
+
+  int rcvSpeedL = 0; //0-127
   int rcvSpeedR = 0;
   bool validCmd = False;
  
  //-------------------------------------------------------// 
 void  setup(){
-  //Starts the serial connection
-  Serial.begin(9600);
-  Serial.write("Hello Dave.");
-  //Turn on motors. (released state)
-  motorFL.run(RELEASE);
-  motorBL.run(RELEASE);
-  motorFR.run(RELEASE);
-  motorBR.run(RELEASE);
+  Serial.println("Welcome to N.A.R.A.");
+  Serial.begin(115200);
+  Serial.println("qik 2s12v10 dual serial motor controller");
+  
+  qik.init();
+  
+  Serial.print("Firmware version: ");
+  Serial.write(qik.getFirmwareVersion());
+  Serial.println();
 }
 //-------------------------------------------------------// 
 void loop(){ 
-  //Reads the incoming command.
-  ReadIncoming();
-  //Executes the command. Fetch boy, fetch!
-  ExecuteCommands();
 
-}
-
-/*--------------------------------------
-  Process the proper axis for sending motor speeds, based on quadrant read.
-  These are the Values that need SENT for proper motion, over the READ Values.
-        (F)    (Y)   (F)
-              (255)
-        IV      |     I
-    X:R Y:L     |  X:L Y:R
-    X:-R  Y:L   |  X:L Y:R
-(-X)(-255)-----------------(255-)(X)
-        (B)     |    (B)
-         III    |     II
-     X:-R Y:-L  | X:-L Y:-R
-     X:-R Y:-L  | X:L  Y:-R
-             (-255)
-              (-Y)
-----------------------------------------*/
-
-//-------------------------------------------------------// 
-void ReadIncoming(){
-  if (Serial.available()) {
-    
-  }
-  while(validCmd == false) {
   //Read the incoming command, 1 byte, into memory to use as a command.
- rcvDirection = Serial.read();
- rcvSpeedL = Serial.read();
- rcvSpeedR = Serial.read();
+  rcvSpeedL = Serial.read();
+  rcvSpeedR = Serial.read();
 
 //Verify valid direction and min/max speed recieved.
- if (rcvDirection == 'F' || 'B' || 'n'){
-  if (rcvSpeedL >= 0 && rcvSpeedL <= 255){
-    if rcvSpeedR >= 0 && rcvSpeedR <= 255) {
-        validCmd = True ;
+//Min and max values for the Qik 12v10 is -127 to 127.
+
+  if (rcvSpeedL >= -127 && rcvSpeedL <= 127){
+    if rcvSpeedR >= -127 && rcvSpeedR <= 127) {
+
+        //  Serial.println("I have a good command!");
+          //There are two different channels on the Qik12v10, M0 and M1. This assumed 0 is left, and 1 is right.
+          qik.setM0Speed(FSpeedL);
+          qik.setM1Speed(FSpeedR);
        }
      }
     }
-  }
 
-}
-//-------------------------------------------------------// 
-void ExecuteCommands(){
-  
-  //Translate the command into a direction. F/B: Fwd/reverse, L/R: Left, Right, l/r: reverse turn.
-  switch(rcvDirection){
-    //Checks the first byte recieved for the direction to start: Forward, Backward or Neutral steer.
-    case 'F':
-    // Reply with the direction intended to go
-      Serial.println("GOING STRAIGHT AT:");
-      Serial.println(rcvSpeedL, rcvspeedR);
-      //Start moving with forward intent.
-      GoForward(rcvSpeedL, rcvSpeedR);
-      break;
-    case 'B':
-    //Reply with the direction to go
-      Serial.println("GOING BACKWARDS AT:");
-      Serial.println(rcvSpeedL, rcvSpeedR);
-      //Start moving with backwards intent.
-      GoBackwards(rcvSpeedL, rcvSpeedR);
-      break;
-    case 'N':
-    // Dat neutral steer.
-      Serial.println("NEUTRAL JUST KICKED IN YO")
-      NeutralSteer(rcvSpeedL, rcvSpeedR);
-      break;
-  }
-}
-//-------------------------------------------------------// 
-//Set motor driver to appropriate fwd speeds.
-void GoForward(int FSpeedL, int FSpeedR){
-  //seperates stop and go command.  Min/Max motor speeds here.
-  
-    motorFL.setSpeed(FSpeedL);
-    motorBL.setSpeed(FSpeedL);
-    motorFR.setSpeed(FSpeedR);
-    motorBR.setSpeed(FSpeedR);
-    motorFL.run(FORWARD);
-    motorFR.run(FORWARD);
-    motorBL.run(FORWARD);
-    motorBR.run(FORWARD);
-}
-//-------------------------------------------------------// 
-void GoBackwards(int FSpeedL, int FSpeedR){
-  //Same as fwd, except reverse.
-    motorFL.setSpeed(FSpeedL);
-    motorBL.setSpeed(FSpeedL);
-    motorFR.setSpeed(FSpeedR);
-    motorBR.setSpeed(FSpeedR);
-    motorFL.run(BACKWARD);
-    motorFR.run(BACKWARD);
-    motorBL.run(BACKWARD);
-    motorBR.run(BACKWARD);
-}
-//-------------------------------------------------------// 
-void NeutralSteer(int FSpeedL, int FSpeedR){
-
-//Neutral Left
-  //Negative X value, positive Y
-if (FSpeedR <= -10 && FSpeedL >= 0){
-    motorFL.setSpeed(FSpeedL);
-    motorFR.setSpeed(FSpeedR);
-    motorBL.setSpeed(FSpeedL);
-    motorBR.setSpeed(FSpeedR);
-    motorFL.run(RELEASE);
-    motorFR.run(FORWARD);
-    motorBL.run(RELEASE);
-    motorBR.run(FORWARD);
-}
-
-//Neutral right
-if (FSpeedL <= -10 && FSpeedR >= 0){
-    motorFL.setSpeed(FSpeedL);
-    motorFR.setSpeed(FSpeedR);
-    motorBL.setSpeed(FSpeedL);
-    motorBR.setSpeed(FSpeedR);
-    motorFL.run(FORWARD);
-    motorFR.run(RELEASE);
-    motorBL.run(FORWARD);
-    motorBR.run(RELEASE);
-}
